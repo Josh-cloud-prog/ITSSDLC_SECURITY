421333750bb885df7945f5e37a7cb10c46b50cf1
fmdlocsin <114983979+fmdlocsin@users.noreply.github.com>
2025-03-29 20:55:50 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@ plugins {
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    id "org.sonarqube" version "6.0.1.5171"
+}
+
+sonar {
+  properties {
+    property "sonar.projectKey", "itssdlc"
+    property "sonar.projectName", "ITSSDLC FINAL OUTPUT"
+  }
 }
