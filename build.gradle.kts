f97116ecfe6f837bbe60f424c1a714813fb4279b
fmdlocsin <felipe_locsin@dlsu.edu.ph>
2025-03-29 23:26:34 +0800

    updatedf

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 
-    id("org.sonarqube") version "6.0.1.5171"
 }
+
+plugins {
+  id("org.sonarqube") version "6.0.1.5171"
+}
+
 
 sonar {
   properties {
